de82d6c2e04370be3e55fd955fee256eb78a1ea2
jill zhang <talktojillzhang@gmail.com>
2018-09-03 07:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ function endGame() {
 
 function moveDodger(e) {
   if (e.which === LEFT_ARROW){
-    e.stop
+    e.stopPropagation()
      window.requestAnimationFrame(moveDodgerLeft())
   }
   if (e.which === RIGHT_ARROW){
